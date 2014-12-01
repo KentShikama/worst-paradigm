ec709cc8908942c27955197314747df834968b82
theworldbright <sushionigiris@gmail.com>
2014-12-01 02:25:31 -0800

    Move ItemService methods to PersonService

--- grails-app/services/experiment/PersonService.groovy
+++ grails-app/services/experiment/PersonService.groovy
@@ -2,14 +2,62 @@ package experiment
 
 class PersonService {
 
-    Person createPet(String id, int age, String gender,
+    List<String> correctWordList = ["shoe","hand","toe","kick","sandals","soccer","yard","walk","ankle","arm","boot","inch","sock","smell","mouth"].toList();
+
+    Person createPerson(String id, int age, String gender, String studyType,
                      ArrayList<String> listOfTrueWordsRecalled, ArrayList<String> listOfFalseWordsRecalled,
-                     int numberOfTrueWordsRecalled, boolean didRecallCriticalLureInListOfWords, boolean didRecallCriticalLureInYesOrNoQuestions) {
+                     int numberOfTrueWordsRecalled, boolean didRecallCriticalLureInFreeRecall) {
+        Person person = new Person(id: id, age: age, gender: gender, studyType: studyType,
+                listOfTrueWordsRecalled: listOfTrueWordsRecalled, listOfFalseWordsRecalled: listOfFalseWordsRecalled,
+                numberOfTrueWordsRecalled: numberOfTrueWordsRecalled, didRecallCriticalLureInFreeRecall: didRecallCriticalLureInFreeRecall);
+        person.save(flush: true, failOnError: true);
+        return person;
+    }
+
+    Person createPerson(String id, int age, String gender,
+                        ArrayList<String> listOfTrueWordsRecalled, ArrayList<String> listOfFalseWordsRecalled,
+                        int numberOfTrueWordsRecalled, boolean didRecallCriticalLureInFreeRecall, boolean didRecallCriticalLureInRecognition) {
         Person person = new Person(id: id, age: age, String: gender,
                 listOfTrueWordsRecalled: listOfTrueWordsRecalled, listOfFalseWordsRecalled: listOfFalseWordsRecalled,
-                numberOfTrueWordsRecalled: numberOfTrueWordsRecalled, didRecallCriticalLureInListOfWords: didRecallCriticalLureInListOfWords, didRecallCriticalLureInYesOrNoQuestions: didRecallCriticalLureInYesOrNoQuestions);
+                numberOfTrueWordsRecalled: numberOfTrueWordsRecalled, didRecallCriticalLureInFreeRecall: didRecallCriticalLureInFreeRecall, didRecallCriticalLureInRecognition: didRecallCriticalLureInRecognition);
         person.save();
         return person;
     }
 
+    List<Item> createItems() {
+        List<Item> itemList = new ArrayList<>();
+        for (String word: correctWordList) {
+            Item item = new Item(word: word, lineDrawingURL: word + "-linedrawing.png", photographURL: word + "-photograph.png");
+            itemList.add(item);
+        }
+        return itemList;
+    }
+
+    List<String> buildWordsCorrectlyRecalledList(List<String> recalledWordList) {
+        List<String> wordsCorrectlyRecalled = new ArrayList<String>();
+        for (String word : correctWordList) {
+            for (String recalledWord : recalledWordList) {
+                if (recalledWord.equals(word)) {
+                    wordsCorrectlyRecalled.add(word);
+                }
+            }
+        }
+        return wordsCorrectlyRecalled;
+    }
+
+    List<String> buildWordsIncorrectlyRecalledList(List<String> recalledWordList) {
+        List<String> wordsIncorrectlyRecalled = new ArrayList<String>();
+        for (String recalledWord : recalledWordList) {
+            boolean isCorrectWord = false;
+            for (String correctWord : correctWordList) {
+                if (recalledWord.equals(correctWord)) {
+                    isCorrectWord = true;
+                }
+            }
+            if (!isCorrectWord) {
+                wordsIncorrectlyRecalled.add(recalledWord);
+            }
+        }
+        return wordsIncorrectlyRecalled;
+    }
 }
